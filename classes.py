1ca7bc60ee8ad0a86eed3e8e1b7b095860d32937
brunomaion <brunosmaion@hotmail.com>
2024-04-16 22:14:41 -0300

    first commit

---
+++ classes.py
@@ -0,0 +1,16 @@
+class Vertice:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+class ListaVertices:
+  def __init__(self, a, b, c):
+    self.v1 = a
+    self.v2 = b
+    self.v3 = c
+
+class Triangulo:
+  def __init__(self, vertices):
+    self.vertices = vertices
+    
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from classes import *
+
+
+v1 = Vertice(0,0)
+v2 = Vertice(20,30)
+v3 = Vertice(40,10)
+
+List1 = ListaVertices(v1, v2, v3)
+
+
+triangulo1 = Triangulo(List1)
+
+
+print(triangulo1.vertices.)
